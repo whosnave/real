e9c2b9368e569d54a8f7b8bc6dc745c97d667ea5
nave <148296413+whosnave@users.noreply.github.com>
2023-11-29 20:40:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,325 @@
+// let num1 = 20
+// let num2 = 20
+
+// if (num1 !== num2) {
+//     console.log("Gak Sama")
+// } else {
+//     console.log("Samma")
+// }
+
+// let value = 11;
+
+// value -= 4;
+// console.log(value)
+
+// value--;
+// console.log(value)
+
+// let manggo = 23
+// let melons = 20
+
+// let result = manggo > melons
+// console.log("Hasilnya adalah " + result)
+// console.log(`Hasilnya adalah ${result}`)
+
+// let letris = 2;
+// console.log(letris <= 2)
+
+// const x = '2'
+// const y = "2"
+
+// const result = x === y;
+// console.log(result)
+
+// let number1 = 2;
+// let number2 = '2';
+// let result = number1 === number2;
+
+// if (number1 === number2) {
+//     document.write(result)
+// } else {
+//     document.write(result)
+// }
+
+// let kaki = 'tiga';
+// kaki == 'dua' ? console.log('true') : console.log('salah')
+
+// let firstName = 'Evan';
+// let lastName = 'Sardi';
+// console.log('Name: ' + firstName + ' ' + lastName)
+// console.log(`Name: ${firstName} ${lastName}`)
+
+// let input = parseInt(prompt('Pilih angka dari 1 - 5'));
+// let isi = '';
+
+// switch (input) {
+//     case 1:
+//         isi = 'Rafi!';
+//         break;
+//     case 2:
+//         isi = 'Radith!';
+//         break;
+//     case 3:
+//         isi = 'Bimantaro!';
+//         break;
+//     case 4:
+//         isi = 'Lusi!';
+//         break;
+//     case 5:
+//         isi = 'Tiko!';
+//         break;
+//     default:
+//         document.write(isi)
+//         break;
+// }if (input != 1-5) {
+//        document.write(`Selamat kamu dapat ${isi}`)
+// }else if (isNaN(input != 1-5))
+//     document.write('tolol')
+
+// let quest = prompt('Do u love me??')
+// let answ = (quest.toUpperCase() == 'YES') ? 'I love you too' : 'fuck you';
+// document.write(`${answ}`)
+
+// for(let i = 0; i <= 5; i++){
+//     console.log(i)
+// }
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i + 1)
+// }
+
+// let promp = confirm('Yakin ingin mengulangi?')
+// let repeat = 0;
+
+// while (promp == true) {
+//     let answ = confirm("yes/no")
+//     repeat++;
+//     if (answ == false) {
+//         promp = false;
+//     }
+// }document.write(`repeated ${repeat - 1}x times`)
+
+// let a = confirm("Apakah kamu yakin tidak mau makan?")
+// let repeat = 0;
+
+// do {
+//     repeat++;
+//     let d = confirm("Yakin?")
+//     if(d == false){
+//         a = false; }
+// } while (a == true); {
+//     document.write(`total ${repeat}x`)
+// }
+
+// FOR OUT
+// let names = ["Bettle", "Don", "Polo"];
+
+// for (let n in names) {
+//     document.write(`${parseInt(n) + 1}. ${names[n]} <br>`);
+// }
+
+// let names = ["Bettle", "Don", "Polo"];
+// let n;
+
+// for (n in names) {
+//     document.write(`${n}. ${names} <br>`)
+// }
+
+// let makanan = ["Sop", "Soto", "Nasi Goreng", "Mie Goreng"];
+// let i;
+
+// for (i in makanan){
+//     document.write(`${i}. ${makanan[i]} <br>`)
+// }
+
+// let names = ["Bettle", "Don", "Polo"];
+
+// names.forEach((nama, n) => {
+//     document.write(`${n + 1}. ${nama} <br>`)
+// });
+// repeat
+// document.write(`Don Pollo<br>`  .repeat(5))
+
+// min foreach dan while
+
+// let arr = ['AYAM','BEBEK','KUCING'];
+// console.log(` Array: ${arr [0]}`);
+
+// let arr = ['Evan', '2', 'INDONESIA', 'LETRIS', 'SISWA']
+// console.log(`${arr[4]} bernama ${arr[0]} berasal dari ${arr[3]} ${arr[2]} ${arr[1]} `);
+
+// for (let mangga = 0; mangga < 20; mangga++) {
+//     console.log(mangga +1);
+// }
+
+// let tek = 'kocak';
+// console.log(`${tek}`);
+
+// for (let mangga = 1; mangga <= 10; mangga++) {
+//     document.write(`Perulangan mangga ke: ${mangga} <br>`)
+// }
+
+// let mangga = confirm('apakah kamu suka mangga?')
+// do{
+//     document.write('suka mangga')
+// }while(mangga != false)
+
+// let operator = prompt('+ - * /')
+// let num1 = prompt('Number 1')
+// let num2 = prompt('Number 2')
+
+// if (operator == '+') {
+//     document.write(`${num1 + num2}`)
+// } else {
+
+// }
+
+// let arr = ['Bayu1', 'Ahmad', 'Bayyu']
+// arr.push("Jesse")
+// arr.push("Jess2e")
+// arr.push("Jess4e")
+// console.log(arr);
+// console.log(`${arr[0]} ${arr[2]}`);
+// console.log(`${arr[1]}`);
+
+// let totalNilai = prompt('Masukan nilai kamu', 0)
+1; //     document.write("<h1>Selamat Anda lulus</h1>")
+// )
+// document.write("Terimakasih sudah menginput nilai")
+
+// let jawaban = parseInt(prompt('Pilih angka 1-2!'));
+// let hadiah = "";
+
+// switch (jawaban) {
+//     case 1:
+//         hadiah = "Pesawat";
+//         break;
+//     case 2:
+//         hadiah = "Kapal";
+//         break
+//     default:
+//         document.write('Salah');
+//         break;
+// }document.write(hadiah)
+
+// let username = prompt("Input your Username");
+// let password = prompt("Input your Password");
+
+// if (username.toUpperCase() == "NAVE") {
+//     if (password.toUpperCase() == "NAVE123") {
+//         document.write(`<h1>SELAMAT DATANG ${username.toUpperCase()}`);
+//         } else {
+//           document.write("Password Salah!");
+//         }
+// } else {
+//   document.write("<h1>Anda belum terdaftar!</h1>");
+// }
+
+// let namaWarna = ['Merah', 'Kuning', 'Hijau', 'Pink', 'Hitam']
+// namaWarna[3] = 'Biru'
+// namaWarna.push('Hitam', 'Putih')
+// delete namaWarna[2];
+// namaWarna.pop() // End
+// namaWarna.shift() // Start
+// namaWarna.splice(1, 2)
+// console.log(namaWarna);
+
+// Method filter berfunsgi untuk menyaring data
+// let angka = [1,2,3,4,5,6,7,8,9,10];
+// const hasilFilter = angka.filter
+// ((items) => {
+//     return items % 2 === 0;
+// });
+// console.log(hasilFilter);
+
+// Method Includes berfungsi untuk cek data Array
+// let warna = ["Merah", "Biru", "Hitam"]
+// let filter = warna.includes("Merah")
+// console.log(filter);
+
+// let alfabet = ["f", "c", "b", "e", "d", "a", "g", "h", "j", "i", "k", "m", "l", "n", "o", "q", "p", "r", "s", "t", "u", "v", "w", "x", "z", "x"]
+// console.log(alfabet.sort());
+// let a = 5.2;
+// let b = 5.6;
+// let c = 5.5;
+// bl = Math.round(c);
+// console.log(bl);
+
+// bla = math.cell(b)
+// console.log(bla);
+
+// let akar = Math.sqrt(225)
+// console.log(akar);
+
+// let x = prompt("x")
+// let n1 = prompt("1")
+// let n2 = prompt("2")
+
+// if (x === "x") {
+//     r = n1 * n2;
+//     document.write(r)
+// } else {
+//     document.write('Error!!')
+
+// let min = Math.min(242, 423, 123, 322);
+// console.log(min);
+// let max = Math.max(242, 423, 123, 322);
+// console.log(max);
+
+// let abs = Math.abs(-5)
+// console.log(abs);
+
+// let shuffle = Math.floor(Math.random()*10) + 1;
+// alert(shuffle)
+
+// function namaFungsi() {
+//   console.log("Ini adalah Deklarasi Function");
+// }
+// namaFungsi();
+// let funct = prompt("namaFungsi")
+// if (funct === "namaFungsi") {
+//     namaFungsi();
+// } else {
+//     alert("Error")
+// }
+
+// const ekspresi = function () {
+//   console.log("Ini adalah Ekpresi Function");
+// };
+// ekspresi();
+
+// const arrow = () => {
+//   console.log("Ini adalah Arrow Function");
+// };
+// arrow();
+
+// let piket = function(){
+//     console.log("Akbar gak piket! :(");
+// }
+// piket();
+
+// let op = prompt("Operator + - * /");
+// let num1 = parseFloat(prompt("Num1"));
+// let num2 = parseFloat(prompt("Num2"));
+
+// if (op === "+") {
+//   r = num1 + num2;
+//   document.write(`<h1>Hasilnya adalah : ${r}</h1>`);
+// } else if (op === "-") {
+//   r = num1 - num2;
+//   document.write(`<h1>Hasilnya adalah : ${r}</h1>`);
+// } else if (op === "*") {
+//   r = num1 * num2;
+//   document.write(`<h1>Hasilnya adalah : ${r}</h1>`);
+// } else if (op === "/") {
+//   r = num1 / num2;
+//   document.write(`<h1>Hasilnya adalah : ${r}</h1>`);
+// } else {
+//   alert("Error");
+// }
+
+let confirm1 = prompt("Did you win? (yes/no)")
+if ( confirm1.toUpperCase() == "YES" ) {
+  document.write("U bussin my Theory U Win let's Play Again!")
+} else 
+  document.write("Pake Khufra aja NGENTOTT")
